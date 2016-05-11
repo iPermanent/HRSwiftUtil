3df4356e46fb89a9a5e39e0e979f1b1e24a01411
henry <henryzhangios@gmail.com>
2016-05-11 16:07:26 +0800

    视频声音合成功能

--- HRSwiftUtil/Utils/HRVideoUtil.swift
+++ HRSwiftUtil/Utils/HRVideoUtil.swift
@@ -18,7 +18,7 @@ enum cutVideoType {
 
 class HRVideoUtil: NSObject {
 
-    internal typealias convertResult = (Bool,String) -> Void
+    internal typealias convertResult = (Bool,String,String) -> Void
     
     /**
      将视频截取为正方形
@@ -32,7 +32,7 @@ class HRVideoUtil: NSObject {
         let asset:AVAsset? = AVAsset.init(URL: NSURL.init(fileURLWithPath: videoPath))
         if asset == nil{
             print("read video with path" + videoPath + "failed")
-            result(false,"input video file is not avaliable")
+            result(false,"input video file is not avaliable","no file output")
             return
         }
         
@@ -80,7 +80,79 @@ class HRVideoUtil: NSObject {
         exporter.exportAsynchronouslyWithCompletionHandler { 
             dispatch_async(dispatch_get_main_queue()){
                 print("exported success")
-                result(true,"exported success")
+                result(true,"exported success",outputPath)
+            }
+        }
+    }
+    
+    /**
+     向视频中加入声音
+     
+     - parameter videoPath: 视频路径
+     - parameter audioPath: 声音路径
+     - parameter result:    完成回调
+     */
+    static func mixAudioToVideo(videoPath:String,audioPath:String,result:convertResult){
+        let mixComposition:AVMutableComposition = AVMutableComposition.init()
+        let audioInputUrl:NSURL! = NSURL.init(fileURLWithPath: audioPath)
+        let videoInputUrl:NSURL! = NSURL.init(fileURLWithPath: videoPath)
+        
+        let docPath:String = NSHomeDirectory().stringByAppendingString("/Documents")
+        let outPutFilePath:String! = docPath.stringByAppendingString("/mixed"+videoPath.componentsSeparatedByString("/").last!)
+        let outPutFileUrl:NSURL = NSURL.init(fileURLWithPath: outPutFilePath)
+        
+        if (NSFileManager.defaultManager().fileExistsAtPath(videoPath) == false){
+            result(false,"video not exist","")
+            return
+        }
+        if (NSFileManager.defaultManager().fileExistsAtPath(audioPath) == false){
+            result(false,"audio not exist","")
+            return
+        }
+        if (NSFileManager.defaultManager().fileExistsAtPath(outPutFilePath) == true){
+            do {
+                try NSFileManager.defaultManager().removeItemAtPath(outPutFilePath)
+            } catch {
+                print(error)
+            }
+        }
+        
+        let audioAsset:AVURLAsset = AVURLAsset.init(URL: audioInputUrl)
+        let audioTimeRange:CMTimeRange = CMTimeRangeMake(kCMTimeZero, audioAsset.duration)
+        let b_compositionAudioTrack:AVMutableCompositionTrack = mixComposition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+        do {
+            try b_compositionAudioTrack.insertTimeRange(audioTimeRange, ofTrack: audioAsset.tracksWithMediaType(AVMediaTypeAudio)[0], atTime: kCMTimeZero)
+        } catch {
+            print(error)
+        }
+        
+        let videoAsset:AVURLAsset = AVURLAsset.init(URL: videoInputUrl)
+        let videoTimeRange:CMTimeRange = CMTimeRangeMake(kCMTimeZero, videoAsset.duration)
+        let compositionVideoTrack:AVMutableCompositionTrack = mixComposition.addMutableTrackWithMediaType(AVMediaTypeVideo, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        if (videoAsset.tracksWithMediaType(AVMediaTypeVideo).count != 0){
+            do {
+                try compositionVideoTrack.insertTimeRange(videoTimeRange, ofTrack: videoAsset.tracksWithMediaType(AVMediaTypeVideo)[0], atTime: kCMTimeZero)
+            } catch {
+                print(error)
+            }
+        }
+        
+        let assetExport:AVAssetExportSession = AVAssetExportSession.init(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality)!
+        assetExport.outputFileType = AVFileTypeMPEG4
+        assetExport.outputURL = outPutFileUrl
+        
+        assetExport.exportAsynchronouslyWithCompletionHandler {
+            let status = assetExport.status
+            switch status{
+            case AVAssetExportSessionStatus.Failed:
+                result(false,"","")
+            case AVAssetExportSessionStatus.Exporting:
+                print("exporting...")
+            case AVAssetExportSessionStatus.Completed:
+                result(true,"",outPutFilePath)
+            default:
+                result(false,"","")
             }
         }
     }
